c633ba3948997b7f0d0cd690fed11bad1f31f66d
Michal Uzdowski <michaluzdowski@iMac-Michal.local>
2020-09-10 21:54:40 +0200

    initial build subtree commit

---
+++ build/precache-manifest.d7518ffe4960563dda8da0ca526c2616.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "72dbef79a9adc07b580cab94423fc00e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7bca54b693924bbc4423",
+    "url": "/static/css/main.92bd934a.chunk.css"
+  },
+  {
+    "revision": "fda7aeacd84abf9db942",
+    "url": "/static/js/2.5de09e7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5de09e7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bca54b693924bbc4423",
+    "url": "/static/js/main.8f3a3964.chunk.js"
+  },
+  {
+    "revision": "bf250259d9fd61dc7dbe",
+    "url": "/static/js/runtime-main.ecec8c93.js"
+  }
+]);
